5b8685305f5289180502aac03d064e8172edc210
noracami <kerke.2011@gmail.com>
2023-09-10 04:51:56 +0800

    🔧 move `config.hosts`

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module HanabiCardGame
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << ".zeabur.app"
 
     # Don't generate system test files.
     config.generators.system_tests = nil
